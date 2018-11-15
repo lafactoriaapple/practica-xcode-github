0f05f48a0b9622fddb60ee49ef2e2731ad571127
JUAN CUENCA ORTIGOSA <info@granasoft.com>
2018-11-15 13:31:55 +0100

    Initial Commit

---
+++ githubenxcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubenxcode
+//
+//  Created by JUAN CUENCA ORTIGOSA on 15/11/2018.
+//  Copyright © 2018 LA FACTORIA APPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
